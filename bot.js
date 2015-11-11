1c7e047f5b2fa8144f48b95774dd5e3ffb32b5c4
batorobe <batorobe@msu.edu>
2015-11-10 20:43:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,75 +5,12 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
-      botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexFG = /^\/fag/; botRegexSC = /^\/SDL/i;
-      botRegexP = /^\/PDL/i; botRegexN = /nigger/i; botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/
-  var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
-                ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
-                "MIA","BUF","SF","WAS","NYJ","TB"]
+      botRegex = /^Kill yourself/i;
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(cool());
+    postMessage("http://www.suicidepreventionlifeline.org/");
     this.res.end();
-  } 
-  else if(request.text && botRegexDL.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/rDos/team/"+request.text.substring(5,8)+"/depthchart");
-    this.res.end();
-  } 
-  else if(request.text && botRegexSalt.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://i.imgur.com/q3ohLtD.gif");
-    this.res.end();
-  } 
-  else if(request.text && botRegexAd.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.kno.com/images/store/knoadvance/overview.png");
-    this.res.end();
-  }
-  else if(request.text && botRegexYub.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.quickmeme.com/img/9f/9f3720469d1ce6c2d20130ed0750935a394df80ffcddec6d16e091d95efea854.jpg");
-    this.res.end();
-  } 
-  else if(request.text && botRegexGTA.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("https://i.groupme.com/220x147.jpeg.a2dd2add32b14fff9e329535186d793c.large");
-    this.res.end();
-  } 
-  else if(request.text && botRegexFG.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://i.imgur.com/qU0yAxg.gif");
-    this.res.end();
-  }
-  else if(request.text && botRegexSC.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/rDos/team/"+request.text.substring(5,8)+"/schedule");
-    this.res.end();
-  }
-  else if(request.text && botRegexP.test(request.text)) {
-    this.res.writeHead(200);
-    var req = request.text.substring(5,request.text.length);
-    var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/rDos/players?name="+rep+"&position=all&team=all");
-    this.res.end();
-  } 
   
-  else if(request.text && botRegexN.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("HARD R ALERT");
-    this.res.end();
-  } 
-  else if(request.text && botRegexTw.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
-    this.res.end();
-  } 
-  else if(request.text && botRegexSb.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.reddit.com/r/mcfdos");
-    this.res.end();
-  } 
   else {
     console.log("don't care");
     this.res.writeHead(200);
